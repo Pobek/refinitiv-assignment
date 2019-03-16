9f02ef4f5207752cc92cf953985465a5a37b5815
Sputters <coevyatar@gmail.com>
2019-03-16 13:09:38 +0200

    Update test script & Add deploy script

--- test_refinitiv_app.sh
+++ test_refinitiv_app.sh
@@ -1,21 +1,5 @@
 #!/bin/sh
 
-echo "---- Start Building Sequence ----"
-
-echo "Getting the image"
-docker build -t refinitiv-app:v1 ./application_host/
-
-echo "Start nginx"
-docker run -d -p 8080:80 -v "$(pwd)/load_balancer/nginx.conf:/etc/nginx/conf.d/default.conf" --name nginx-load-balance nginx
-
-echo "Start refinitiv-app 1"
-docker run -d --name refinitiv1 refinitiv-app:v1
-
-echo "Start refinitiv-app 2"
-docker run -d --name refinitiv2 refinitiv-app:v1
-
-echo "---- End Building Sequence ----"
-
 echo "---- Start Testing Sequence ----"
 
 sleep 2
